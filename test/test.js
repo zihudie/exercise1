3aa2a5dbaaeff62deef29e8df9aabe7eac3800dc
zihudie <452637718@qq.com>
2018-06-15 17:08:03 +0800

    wuyaofang_exercise1
    mocha test1 study

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
-var assert = require('assert')
+const assert = require('assert')
+
+
+assert.equal(-1, [1, 2, 3].indexOf(5),"成功了")
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(5),"成功了")
     })
   })
 })
+
 
 describe('assert', function () {
   it('a和b应当深度相等', function () {
@@ -21,14 +25,25 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
+    
   })
-
+ 
   it('可以捕获并验证函数fn的错误', function () {
-    function fn() {
-      xxx;
+    function fn(data) {
     }
+  
     // 修改下面代码使得满足测试描述
-    fn()
+      const { message } = new assert.AssertionError({
+        actual: 1,
+        expected: 2,
+        operator: 'test'
+      });
+      try {
+        fn()
+      } catch (err) {
+        assert(err instanceof assert.AssertionError)
+        assert.strictEqual(err.message, message)
+      }
   })
 })
